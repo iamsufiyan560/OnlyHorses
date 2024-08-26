77016def80fc03f4fb22f0f7c54b2793d5767c64
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-08-26 22:00:53 +0530

    logout and themeprovider

--- src/components/home/home-screen/HomeScreen.tsx
+++ src/components/home/home-screen/HomeScreen.tsx
@@ -1,5 +1,12 @@
+"use client";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div>
+      <LogoutLink>LogOut</LogoutLink>
+    </div>
+  );
 };
 
 export default HomeScreen;
